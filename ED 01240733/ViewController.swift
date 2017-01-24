7175f5af488e89937c17d5a2ea55884fbbb52fe2
Ron Sala <ron.sala@gmail.com>
2017-01-24 07:33:55 -0500

    Initial Commit

---
+++ ED 01240733/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ED 01240733
+//
+//  Created by RONALD SALA on 1/24/17.
+//  Copyright © 2017 RON SALA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
