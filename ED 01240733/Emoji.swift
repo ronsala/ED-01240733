35e11fa295c1fbd031ebaeb27f8b22bcbe10e320
Ron Sala <ron.sala@gmail.com>
2017-02-05 05:37:04 -0500

    L52: Classes in emoji dictionary (shows error when clicked, just as in lecture)

---
+++ ED 01240733/Emoji.swift
@@ -0,0 +1,14 @@
+//
+//  Emoji.swift
+//  ED 01240733
+//
+//  Created by RONALD SALA on 2/5/17.
+//  Copyright © 2017 RON SALA. All rights reserved.
+//
+
+class Emoji {
+    var stringEmoji = ""
+    var definition = ""
+    var category = ""
+    var birthYear = 0
+}
--- ED 01240733/ViewController.swift
+++ ED 01240733/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
 
     @IBOutlet weak var dacooltableview: UITableView!
     
-    var emojis = ["😅","🦊","🍌","🤾🏿‍♂️","❤️"]
+    var emojis : [Emoji] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +20,7 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         
         dacooltableview.dataSource = self
         dacooltableview.delegate = self
+        emojis = makeEmojiArray()
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -29,7 +30,8 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         print(indexPath.row)
         let cell = UITableViewCell()
-        cell.textLabel?.text = emojis[indexPath.row]
+        let emoji = emojis[indexPath.row]
+        cell.textLabel?.text = emoji.category
         return cell
     }
     
@@ -49,6 +51,39 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         // Dispose of any resources that can be recreated.
     }
 
-
+    func makeEmojiArray() -> [Emoji] {
+        let emoji1 = Emoji()
+        emoji1.stringEmoji = "😅"
+        emoji1.birthYear = 2010
+        emoji1.category = "Smiley"
+        emoji1.definition = "Sweating from laughing"
+    
+        let emoji2 = Emoji()
+        emoji2.stringEmoji = "🦊"
+        emoji2.birthYear = 2009
+        emoji2.category = "Animal"
+        emoji2.definition = "Fox running"
+        
+        let emoji3 = Emoji()
+        emoji3.stringEmoji = "🍌"
+        emoji3.birthYear = 2011
+        emoji3.category = "Object"
+        emoji3.definition = "Slip and fall hazard"
+        
+        let emoji4 = Emoji()
+        emoji4.stringEmoji = "🤾🏿‍♂️"
+        emoji4.birthYear = 2004
+        emoji4.category = "Person"
+        emoji4.definition = "Pelé"
+        
+        let emoji5 = Emoji()
+        emoji5.stringEmoji = "❤️"
+        emoji5.birthYear = 2015
+        emoji5.category = "Object"
+        emoji5.definition = "B my love"
+    
+    
+        return [emoji1, emoji2, emoji3, emoji4, emoji5]
+        }
+    
 }
-
